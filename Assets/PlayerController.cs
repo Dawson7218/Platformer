63c0d9eb51d43f67ecc7a590b5dd4f259d8c9d29
Dawson7218 <109839047+Dawson7218@users.noreply.github.com>
2026-09-15 10:28:07 -0700

    added direction turn script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,12 +14,27 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rb;
     private float moveInput;
+    private Animator animator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("is_walking", Mathf.Abs(moveInput));
+        if (moveInput > 0.1f)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else if (moveInput < -0.1f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
     }
 
     // Update is called once per frame
